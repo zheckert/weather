ec7d375eb3756a3779a4509e75f690ad8c957aaf
Zach Heckert <zach@zachheckert.com>
2021-03-10 14:32:07 -0700

    Put a call in there. added dotenv to keep api key safe. call doesnt work, need to fix.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
+import axios from "axios"
 import React from "react"
+require('dotenv').config()
 
 //https://material-ui.com/getting-started/installation/
+
+//https://www.weatherapi.com/docs/
+
+const getWeather = () => {
+    axios.get("http://api.weatherapi.com/", {key: process.env.APIKEY})
+    .then((response) => {
+        console.log(response.data)
+    })
+    .catch((error => console.log(error)))
+}
 
 export const App = () => {
     return(
         <div>
             It's the weather, man. We'll use an api. Also MUI, so make sure we're not using the divs
+            <button onClick={()=> getWeather()}>GET IT</button>
         </div>
         
     )
