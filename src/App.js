1c287592495ce4bd1c81194b9641325d3446a047
Zach Heckert <zach@zachheckert.com>
2021-03-11 16:08:14 -0700

    I got the calls working. I need to figure out a way to protect my API key, since dotenv might be a server only solution or something. Or it's late and I can't figure it out. I need to add a text field and capture user data and let that define the search parameters. Then I need to add more functionality

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
 import axios from "axios"
-import React from "react"
+import React, { useState } from "react"
 require('dotenv').config()
 
 //https://material-ui.com/getting-started/installation/
 
 //https://www.weatherapi.com/docs/
+// console.log("api key test", process.env.APIKEY)
 
-const getWeather = () => {
-    axios.get("http://api.weatherapi.com/", {key: process.env.APIKEY})
-    .then((response) => {
-        console.log(response.data)
-    })
-    .catch((error => console.log(error)))
-}
 
 export const App = () => {
+    
+    const [ weather, setWeather ] = useState("")
+
+    const getWeather = () => {
+        // axios.get("https://vschool-cors.herokuapp.com?url=http://api.weatherapi.com/v1/current.json", {key: process.env.APIKEY}, {q: "orem"})
+        axios.get(`https://vschool-cors.herokuapp.com?url=http://api.weatherapi.com/v1/current.json?key=${process.env.APIKEY}&q=London`)
+        .then((response) => {
+        //    
+        // console.log(response.data)
+            setWeather(response.data.current.feelslike_f)
+        })
+        .catch((error => console.log(error)))
+    }
+
     return(
+        <>
         <div>
             It's the weather, man. We'll use an api. Also MUI, so make sure we're not using the divs
             <button onClick={()=> getWeather()}>GET IT</button>
         </div>
-        
+        <div>
+            {weather}
+        </div>
+        </>
     )
 }
