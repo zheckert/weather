8fa3a424682b0f5827ad4108cd6b8ff9cf602fa1
Zach Heckert <zach@zachheckert.com>
2021-03-12 10:40:28 -0700

    Trying to fix the git mess I made

--- src/App.js
+++ src/App.js
@@ -13,8 +13,8 @@ export const App = () => {
         axios.get(`https://vschool-cors.herokuapp.com?url=http://api.weatherapi.com/v1/current.json?key=${process.env.REACT_APP_APIKEY}&q=Utah`)
         .then((response) => {
         //    
-        // console.log(response.data)
-            setWeather(response.data.current.feelslike_f)
+        console.log(response.data)
+            // setWeather(response.data.current.feelslike_f)
         })
         .catch((error => console.log(error)))
     }
@@ -26,6 +26,7 @@ export const App = () => {
             <button onClick={()=> getWeather()}>GET IT</button>
         </div>
         <div>
+
             {weather}
         </div>
         </>
