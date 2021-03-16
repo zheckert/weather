018b97230025873e3a74dc3d9363c164a608350f
Zach Heckert <zach@zachheckert.com>
2021-03-16 16:14:01 -0600

    short circuited weather card so it wont break before inputs are taken. used react hook form to capture inputs. swapped out hardcoded city in api call for those inputs. still need to handle errors and use a mui form or at least style this abomination

--- src/App.js
+++ src/App.js
@@ -1,38 +1,59 @@
 import axios from "axios"
 import React, { useState } from "react"
+import { useForm } from "react-hook-form"
 
 import Card from "@material-ui/core/Card"
 import Container from "@material-ui/core/Container"
-import Button from "@material-ui/core/Button"
-import Paper from "@material-ui/core/Paper"
+// import Button from "@material-ui/core/Button"
+// import Paper from "@material-ui/core/Paper"
 
 //https://material-ui.com/getting-started/installation/
 
 //https://www.weatherapi.com/docs/
 
 export const App = () => {
+    const { register, handleSubmit, errors } = useForm()
+    const [ city, setCity ] = useState()
+    
+    const onSubmit = (data) => {
+        setCity(data.city)
+        getWeather(city)
+    }
     
     const [ weather, setWeather ] = useState("")
 
-    const getWeather = () => {
-        axios.get(`https://vschool-cors.herokuapp.com?url=http://api.weatherapi.com/v1/current.json?key=${process.env.REACT_APP_APIKEY}&q=Utah`)
+    const getWeather = (city) => {
+        axios.get(`https://vschool-cors.herokuapp.com?url=http://api.weatherapi.com/v1/current.json?key=${process.env.REACT_APP_APIKEY}&q=${city}`)
         .then((response) => {
             setWeather(response.data)
-            // console.log("weather", response.data)
         })
         .catch((error => console.log(error)))
     }
 
-    return(
-        
+    return(    
         <>
             <Container>
-                <Paper>
+                    <div>
+                        <form onSubmit={handleSubmit(onSubmit)}>
+                            <input
+                                type="city"
+                                placeholder="What city?"
+                                name="city"
+                                ref={register({required: true})}
+                            >
+                            </input>
+                            {errors.city && <p>Please enter a city!</p>}
+                            <input type="submit"/>
+                        </form>
+                    </div>
+                {/* <Paper>
                     <Button onClick={()=> getWeather()}>Get It!</Button>
-                </Paper>
-                <Card>
-                    In {weather.location.name}, it's currently {weather.current.temp_f}° Fahrenheit.                    
-                </Card>
+                </Paper> */}
+                { weather && 
+                    <Card>
+                        In {weather.location.name}, it's currently {weather.current.temp_f}° Fahrenheit.                    
+                    </Card>
+                }
             </Container>
         </>
     )
