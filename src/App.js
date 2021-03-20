94a361dd5a42d9b3a08645a48340a17e82500e10
Zach Heckert <zach@zachheckert.com>
2021-03-20 10:18:50 -0600

    Added todo list. Got a svg background working but not all the way, so need to finish that

--- src/App.js
+++ src/App.js
@@ -27,10 +27,25 @@ const useStyles = makeStyles((theme) => ({
 		alignItems: "center",
 		justifyContent: "center",
     },
-    whatever: {
+    mainCard: {
         padding: "20px"
+    },
+    background: {
+        backgroundColor: "#ffffff",
+        backgroundImage: `url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='100%25' height='100%25' %3E%3Cdefs%3E%3ClinearGradient id='a' x1='0' x2='0' y1='0' y2='1'%3E%3Cstop offset='0' stop-color='%231928ff'/%3E%3Cstop offset='1' stop-color='%2314e4ff'/%3E%3C/linearGradient%3E%3C/defs%3E%3Cpattern id='b' width='24' height='24' patternUnits='userSpaceOnUse'%3E%3Ccircle fill='%23ffffff' cx='12' cy='12' r='12'/%3E%3C/pattern%3E%3Crect width='100%25' height='100%25' fill='url(%23a)'/%3E%3Crect width='100%25' height='100%25' fill='url(%23b)' fill-opacity='0.1'/%3E%3C/svg%3E")`,
+        backgroundAttachment: "fixed",
+        backgroundSize: "cover",
     }
 }))
+
+//TODO
+//Need to submit form if enter key is pressed
+//Need to alert user to errors caused by entering invalid information
+//Need to make it so that the submit button is a button and not an input
+//On top of making things look nice in MUI, you need to add a cool background so I don't want to vomit anymore
+//before deployment make sure to move API key to Netlify's key stuff
+
+
 
 export const App = () => {
     const styles = useStyles()
@@ -48,16 +63,16 @@ export const App = () => {
     const getWeather = (city) => {
         axios.get(`https://vschool-cors.herokuapp.com?url=http://api.weatherapi.com/v1/current.json?key=${process.env.REACT_APP_APIKEY}&q=${city}`)
         .then((response) => {
-            setWeather(response.data)
-            // console.log(response.data)
-        })
+                setWeather(response.data)
+                // console.log("response.data", response.data)
+            })
         .catch((error => console.log(error)))
     }
 
     return(    
-        <>
+        < div className={styles.background}>
             <Container className={styles.root}>
-                    <Card className={styles.whatever}>
+                    <Card className={styles.mainCard}>
                         <Typography>Please enter the city you'd like data for below:</Typography>
                         <form onSubmit={handleSubmit(onSubmit)}>
                             <input
@@ -80,6 +95,6 @@ export const App = () => {
                     <Button onClick={()=> getWeather()}>Get It!</Button>
                 </Paper> */}
             </Container>
-        </>
+        </div>
     )
 }
